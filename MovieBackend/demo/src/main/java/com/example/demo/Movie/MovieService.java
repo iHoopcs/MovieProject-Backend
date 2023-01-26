2b7c6571e4734b382770cb6b529cb35c3fa925b9
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-01-26 15:57:55 -0500

    Added Url Attribute to Movie
    Movie contains url attribute

--- MovieBackend/demo/src/main/java/com/example/demo/Movie/MovieService.java
+++ MovieBackend/demo/src/main/java/com/example/demo/Movie/MovieService.java
@@ -14,70 +14,80 @@ public class MovieService {
                         "The Conjuring",
                         "Horror",
                         4,
-                        "R"
+                        "R",
+                        "https://www.youtube.com/embed/VFsmuRPClr4"
                 ),
                 new Movie(
                         3l,
                         "Up",
                         "Adventure",
                         2,
-                        "PG"
+                        "PG",
+                        "https://www.youtube.com/embed/HWEW_qTLSEE"
                 ),
                 new Movie(
                         3l,
                         "Transformers",
                         "Action",
                         5,
-                        "PG13"
+                        "PG13",
+                        "https://www.youtube.com/embed/v8ItGrI-Ou0"
                 ),
                 new Movie(
                         3l,
                         "Star Wars The Empire Strikes Back",
                         "Action",
                         4,
-                        "PG13"
+                        "PG13",
+                        "https://www.youtube.com/embed/JNwNXF9Y6kY"
                 ),
                 new Movie(
                         3l,
                         "Tron",
                         "Action",
                         5,
-                        "PG13"
+                        "PG13",
+                        "https://www.youtube.com/embed/L9szn1QQfas"
                 ),
                 new Movie(
                         3l,
                         "Toy Story",
                         "Coming of Age",
                         1,
-                        "PG"
+                        "PG",
+                        "https://www.youtube.com/embed/v-PjgYDrg70"
                 ),
                 new Movie(
                         3l,
                         "One Piece",
                         "Anime",
                         5,
-                        "PG13"
+                        "PG13",
+                        "https://www.youtube.com/embed/S8_YwFLCh4U"
                 ),
                 new Movie(
                         3l,
                         "Breaking Bad",
                         "TV Show",
                         5,
-                        "Parental Discretion Advised"
+                        "Parental Discretion Advised",
+                        "https://www.youtube.com/embed/HhesaQXLuRY"
                 ),
                 new Movie(
                         3l,
                         "Money Heist",
                         "TV Show",
                         5,
-                        "Parental Discretion Advised"
+                        "Parental Discretion Advised",
+                        "https://www.youtube.com/embed/_InqQJRqGW4"
                 ),
                 new Movie(
                         3l,
                         "Superbad",
                         "Comedy",
                         3,
-                        "PG13"
+                        "PG13",
+                        "https://www.youtube.com/embed/4eaZ_48ZYog"
                 )
 
         );
